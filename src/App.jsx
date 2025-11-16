34d5a42daefde5cbaaac8e7ca50de13cab265579
TalhaShaikh150 <tssmart233@gmail.com>
2025-11-16 21:19:42 +0500

    initialize project with Tailwind CSS setup and React Hook Form integration for form management

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import HookForm from './HookForm'
+
+function App() {
+
+  return (
+    <>
+      <HookForm/>
+    </>
+  )
+}
+
+export default App
---
+++ src/HookForm.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HookForm = () => {
+  return <div className="text-5xl text-red-500">HookForm</div>;
+};
+
+export default HookForm;
